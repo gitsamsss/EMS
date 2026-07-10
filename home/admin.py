61c8eee17a73b07e4cb3e30e1a2ce2fca703b737
Samir Thapa <thapasamirst490@gmail.com>
2026-07-10 19:47:09 +0545

    my message

---
+++ home/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Event , Bookings , ExpiredEvent
+
+
+# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display=['name','seats']
+admin.site.register(Event,EventAdmin)
+admin.site.register(Bookings)
+
+
+from django.utils import timezone
+class ExpiredEventAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(dnt__lt=timezone.now())
+    
+admin.site.register(ExpiredEvent,ExpiredEventAdmin)
